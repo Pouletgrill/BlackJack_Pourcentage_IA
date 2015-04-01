6a4dc797dc3d9811485150341277a07f220f6e1e
PouletGrill <xavierb.2006@hotmail.com>
2015-04-01 08:25:07 -0400

    Debut

---
+++ BlackJack/BlackJack/Carte.cs
@@ -0,0 +1,53 @@
+﻿/////////////////////
+// Class Carte     //
+// Xavier Brosseau //
+// Charlie Laplante//
+/////////////////////
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+   class Carte
+   {
+      Image cartePNG_ = null;
+      String path_ = null;
+      int valeur_;
+
+      public Carte(String Path)
+      {
+         cartePNG_ = Image.FromFile(Path);
+         path_ = Path;
+      }
+      public Carte(String Path, int Valeur)
+      {
+         cartePNG_ = Image.FromFile(Path);
+         path_ = Path;
+         valeur_ = Valeur;
+      }
+
+      public void SetValeur(int Valeur)
+      {
+         valeur_ = Valeur;
+      }
+
+      public Image GetImage()
+      {
+         return cartePNG_;
+      }
+
+      public String GetPath()
+      {
+         return path_;
+      }
+
+      public int GetValeur()
+      {
+         return valeur_;
+      }
+   }
+}
---
+++ BlackJack/BlackJack/Joueur.cs
@@ -0,0 +1,133 @@
+﻿/////////////////////
+// Class Joueur    //
+// Xavier Brosseau //
+// Charlie Laplante//
+/////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+   class Joueur
+   {
+      int TotalPoint = 0;
+      String Nom_;
+      float stats_ = 100;
+      int Cpu_Level_ = -1;
+      bool Joue_ = true;
+      bool Compte_carte_ = false;
+      string pense_ = "";
+
+      public string AfficherDetails()
+      {
+         return pense_;
+      }
+      public Joueur(String Nom)
+      {
+         Nom_ = Nom;
+      }
+      public Joueur(int Cpu_Level, bool CompteCarte, String Nom)
+      {
+         Nom_ = Nom;
+         Cpu_Level_ = Cpu_Level;
+         Compte_carte_ = CompteCarte;
+      }
+
+      public float GetStat()
+      {
+         return stats_;
+      }
+      public int GetTotal()
+      {
+         return TotalPoint;
+      }
+      public string GetNom()
+      {
+         return Nom_;
+      }
+      public int GetCpuLevel()
+      {
+         return Cpu_Level_;
+      }
+      public void SetTotal(int Total)
+      {
+         TotalPoint = Total;
+      }
+      public void ArreteDeJouer()
+      {
+         Joue_ = false;
+      }
+      public bool JoueEncore()
+      {
+         return Joue_;
+      }
+      public bool AIJoueEncore()
+      {
+         //On vérifie si on continue ou pas
+         if (JoueEncore() && GetCpuLevel() == 1 && GetStat() < 50)//Courageux
+         {
+            ArreteDeJouer();
+            pense_ += Nom_ + ": Arrêt Courageux à " + stats_.ToString() + "%\n";
+         }
+         else if (JoueEncore() && GetCpuLevel() == 2 && GetStat() < 65)//Moyen
+         {
+            ArreteDeJouer();
+            pense_ += Nom_ + ": Arrêt Moyen à " + stats_.ToString() + "%\n";
+         }
+         else if (JoueEncore() && GetCpuLevel() == 3 && GetStat() < 80)//Prudent
+         {
+            ArreteDeJouer();
+            pense_ += Nom_ + ": Arrêt Prudent à " + stats_.ToString() + "%\n";
+         }
+         else if (JoueEncore())
+         {
+            pense_ += Nom_ + ": continue a jouer à " + stats_.ToString() + "%\n";
+         }
+         return JoueEncore();
+      }
+
+      //si les conditions ne sont plus favorable, le joueur s'arrête de lui même
+      public void CalculerStat(List<Carte> paquet, int IndexCarte)
+      {
+         //si le total est trop bas
+         if (GetTotal() > 10)
+         {
+            if (Compte_carte_)//Avec comptage de cartes
+            {
+               float nbChance = 0;
+               for (int i = IndexCarte; i < paquet.Count(); i++)
+               {
+                  if (paquet[i].GetValeur() <= (21 - GetTotal()))
+                  {
+                     nbChance += 1;
+                  }
+                  else if (paquet[i].GetValeur() == 11 && 1 <= (21 - GetTotal()))
+                  {
+                     nbChance += 1;
+                  }
+               }
+               stats_ = (nbChance / (paquet.Count() - IndexCarte)) * 100;
+            }
+            else //Sans comptage de cartes
+            {
+               float nbChance = 0;
+               for (int i = 0; i < paquet.Count(); i++)
+               {
+                  if (paquet[i].GetValeur() <= (21 - GetTotal()))
+                  {
+                     nbChance += 1;
+                  }
+                  else if (paquet[i].GetValeur() == 11 && 1 <= (21 - GetTotal()))
+                  {
+                      nbChance += 1;
+                  }
+               }
+               stats_ = (nbChance / paquet.Count()) * 100;
+            }
+         }
+      }
+   }
+}
